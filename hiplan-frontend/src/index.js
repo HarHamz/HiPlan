6498fc3aa1d76b0b2d8ae539d37485d9c4092253
arya468 <aryapandya88@gmail.com>
2025-06-02 20:59:17 +0700

    penambahan page about

--- hiplan-frontend/src/index.js
+++ hiplan-frontend/src/index.js
@@ -2,10 +2,12 @@ import "./components/navbar-component.js";
 import "./components/footer-component.js";
 import "./assets/styles/styles.css";
 import { HomeView } from "./views/HomeView";
+import { AboutView } from "./views/AboutView";
 import { LoginView } from "./views/LoginView";
 import { RegisterView } from "./views/RegisterView";
 import { MountainModel } from "./models/MountainModel";
 import { HomePresenter } from "./presenters/HomePresenter";
+import { AboutPresenter } from "./presenters/AboutPresenter";
 import { LoginPresenter } from "./presenters/LoginPresenter";
 import { RegisterPresenter } from "./presenters/RegisterPresenter";
 
@@ -22,6 +24,9 @@ document.addEventListener("DOMContentLoaded", () => {
     } else if (hash === "#register") {
       const registerView = new RegisterView();
       new RegisterPresenter(registerView);
+    } else if (hash === "#about") {
+      const aboutView = new AboutView();
+      new AboutPresenter(aboutView, model);
     } else {
       const homeView = new HomeView();
       new HomePresenter(homeView, model);
